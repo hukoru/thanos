a49442f74f11216885998931b3b87119b26601c3
hukoru+1-at-580984682628 <N0gGUYPyj1gWPrX7JSuwIBAOuWDl7F++GSPmIr3zuhc=>
2018-11-28 11:06:48 +0900

    build gradle 정리

--- api/src/main/java/com/thanos/api/domain/Recipe.java
+++ api/src/main/java/com/thanos/api/domain/Recipe.java
@@ -4,11 +4,9 @@ import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-/**
- * Created by jt on 6/13/17.
- */
+
 @Entity
-public class Recipe {
+public class Recipe extends Auditable<Long> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
